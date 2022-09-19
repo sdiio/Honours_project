8353689d8fc87e0353f443059595c7acdb92ff08
sdiio <104544211+sdiio@users.noreply.github.com>
2022-09-19 13:06:50 +1000

    Create 2_minimum depth_per_individual.sh

---
+++ filtering/2_minimum depth_per_individual.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -l ncpus=96
+#PBS -l mem=100gb
+#PBS -l jobfs=70gb
+#PBS -q normal
+#PBS -P ht96
+#PBS -l walltime=24:00:00
+#PBS -N name
+#PBS -l storage=scratch/ht96+gdata/ht96
+
+IN="/g/data/ht96/DiIorio_UQ/scratch/4_local_PCA/"
+OUT="/g/data/ht96/DiIorio_UQ/scratch/4_local_PCA/"
+
+#Minimum depth per individual
+#/home/564/si7912/vcftools --gzvcf ${IN}/finalpopMQ.vcf.gz.recode.vcf --minDP 1 --recode --recode-INFO-all --out ${OUT}/finalpopMQMD
