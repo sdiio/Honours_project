e6cda8b4d59b81b550f0e1021a83e3f9e9c8b2d7
sdiio <104544211+sdiio@users.noreply.github.com>
2022-09-19 11:46:13 +1000

    Update and rename Haplotype Caller to haplotype_caller.sh
    Call variant per sample

---
+++ variant_calling/haplotype_caller.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+module load gatk
+
+#create variables and directories for everything to go into
+
+OUTPUT_DIRECTORY="/g/data/ht96/DiIorio_UQ/scratch/variant_calling/step_1_results"
+BAMFILE="/g/data/ht96/DiIorio_UQ/wgsdata/prepped_data/$1_rln.mdup.cln.bam"
+REFERENCE="/scratch/ht96/si7912/SPD_CN1K_CtgRN.fasta"
+
+gatk --java-options "-Xmx48g" HaplotypeCaller --reference ${REFERENCE} --input ${BAMFILE} --output ${OUTPUT_DIRECTORY}/${1}.g.vcf.gz --create-output-variant-index true --emit-ref-confidence GVCF
+
