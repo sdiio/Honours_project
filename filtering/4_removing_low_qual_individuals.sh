7bce7d080dbcd215cf6dbd38841fa3a57d1a2e45
sdiio <104544211+sdiio@users.noreply.github.com>
2022-10-04 10:42:40 +1000

    Update 4_removing_low_qual_individuals.sh

--- filtering/4_removing_low_qual_individuals.sh
+++ filtering/4_removing_low_qual_individuals.sh
@@ -11,6 +11,9 @@
 IN="/g/data/ht96/DiIorio_UQ/scratch/4_local_PCA/"
 OUT="/g/data/ht96/DiIorio_UQ/scratch/4_local_PCA/"
 
+#creates the file in which there are missing individuals 
 /home/564/si7912/vcftools --vcf ${IN}/finalpopMQMDMiss.recode.vcf --missing-indv --out ${OUT}/out.imiss
+#nominates the threshold for the missing data allowed
 mawk '$5 > 0.4' /g/data/ht96/DiIorio_UQ/scratch/3_SNP_filtering/out.imiss.imiss | cut -f1 > ${OUT}/lowDP.indv
+#uses the file list created above to remove individuals of low quality
 /home/564/si7912/vcftools --vcf ${IN}/finalpopMQMDMiss.recode.vcf --remove lowDP.indv --recode --recode-INFO-all --out ${OUT}/finalpopMQMDMissLQ
