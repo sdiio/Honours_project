60a8c8679bcd5c8d167c55b63dc9b0849f94e501
sdiio <104544211+sdiio@users.noreply.github.com>
2022-10-04 10:41:26 +1000

    Update 3_missing_data_per_site.sh

--- filtering/3_missing_data_per_site.sh
+++ filtering/3_missing_data_per_site.sh
@@ -11,4 +11,5 @@
 IN="/g/data/ht96/DiIorio_UQ/scratch/4_local_PCA/"
 OUT="/g/data/ht96/DiIorio_UQ/scratch/4_local_PCA/"
 
+#removing missing data per site, in this instance at a level of 0.5
 /home/564/si7912/vcftools --gzvcf ${IN}/finalpopMQMD.recode.vcf --max-missing 0.5 --recode --recode-INFO-all --out ${OUT}/finalpopMQMDMiss
